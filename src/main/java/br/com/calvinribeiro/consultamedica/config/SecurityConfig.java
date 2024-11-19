e7d05cf34db7313b2e1f9eb35a13a260c7f7e2aa
calvinribeiro <calvinribeirouniesp1@gmail.com>
2024-11-18 21:04:53 -0300

    SECURITY

--- src/main/java/br/com/calvinribeiro/consultamedica/config/SecurityConfig.java
+++ src/main/java/br/com/calvinribeiro/consultamedica/config/SecurityConfig.java
@@ -11,6 +11,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -23,8 +28,8 @@ public class SecurityConfig {
     };
 
     private static final String[] PUBLIC_MATCHERS_GET = {
-                "/usuarios/**",
-                "/consultas/**"
+            "/usuarios/**",
+            "/consultas/**"
     };
     private static final String[] PUBLIC_MATCHERS_POST = {
             "/usuarios/**",
@@ -40,7 +45,6 @@ public class SecurityConfig {
             "/usuarios/**",
             "/consultas/**"
     };
-<<<<<<< HEAD
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -55,8 +59,6 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
-=======
->>>>>>> origin/main
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -70,4 +72,18 @@ public class SecurityConfig {
                 .withUser("Fernanda").password(passwordEncoder.encode("121212")).roles("SECRETARIA");
 
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigrationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
